a00a9abadb6acf105743b7b75b3c061d6afe2dff
Luis Medina <luis.medina@nuance.com>
2022-01-24 21:16:55 -0600

    "First Commit"

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=nK80P5qnt6Yq4QNvUzGJIsQL48hQ7U77&q=${encodeURI(category)}&limit=5`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    
+    const gifs = data.map(img =>{
+        return {
+            id:img.id,
+            title:img.title,
+            url: img.images?.downsized_medium.url
+
+        }
+    })
+    return gifs;
+}
